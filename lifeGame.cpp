89014075ca6f9fc4c66d8493d97e306762bdbfa5
Mannon <1223431887@qq.com>
2021-03-31 23:33:20 +0800

    把10*10map改为12*12map，也就是在10*10map的四周加上0

--- lifeGame.cpp
+++ lifeGame.cpp
@@ -5,35 +5,53 @@
 
 using namespace std;
 
-void show_map(int life_map[][10])
+void show_map(int life_map[][12])
 {
-    for(int y=9; y>=0; y--)
+    for(int y=10; y>=1; y--)
     {
-        for(int x=0; x<10; x++)
+        for(int x=1; x<11; x++)
         {
-            cout<<" "<<life_map[x][y];
+            cout<<" "<<life_map[y][x];
         }
         cout<<endl;
     }
 }
 
+/*
+游戏规则：
+
+接触位_8位：
+1 2 3
+4 * 5
+6 7 8
+
+新生：*为0，接触位有1~2位，*下一轮变为1.
+(*为0，接触位有3~8位，*下一轮不变)
+延续：*为1，接触位有1~5位，*下一轮不变。
+死亡：*为1，接触位有0或6~8位，*下一轮变为0.
+*/
+
 int main()
 {
 
-    int life_map[10][10];
+    int life_map[12][12];
     int x, y;
 
     // 设置种子
     srand( (unsigned)time(NULL) );
 
-    int n = 100;
-    while (n--)
+    for(y=11; y>=0; y--)
     {
-        x = n / 10;
-        y = n % 10;
-        life_map[x][y] = rand() % 2;//随机生成map，值0，1
-        // life_map[x][y] = x*10 + y;
-    }
+        for(x=0; x<12; x++)
+        {
+            if(y==0||y==11)
+                life_map[y][x] = 0;
+            else if (x==0||x==11)
+                life_map[y][x] = 0;
+            else
+                life_map[y][x] = rand()%2;
+        }
+    }    
     
     show_map(life_map);
 
